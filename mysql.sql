efc5da5b58e712c2133ff372bda6bcdb9f566996
ivan820819 <ivan820819@qq.com>
2014-10-02 11:00:50 +0800

    add

--- mysql.sql
+++ mysql.sql
@@ -18,11 +18,13 @@ CREATE TABLE `wxp_user` (
   `user_regdate` datetime NOT NULL COMMENT '用户注册日期',
   `user_status` enum('1','0') NOT NULL COMMENT '用户状态，1是启用，0是停用',
   `user_printer` int(11) unsigned NOT NULL COMMENT '用户来源设备ID，关联printer表的printer_id字段',
+  `user_weixin` varchar(50) NOT NULL COMMENT '公众号帐号，关联weixin表weixin_number字段',
+  `user_follow` enum('0','1') NOT NULL COMMENT '用户关注公众号状态，0是未关注，1是已关注',
   PRIMARY KEY (`id`),
   UNIQUE KEY user_id (user_id)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='用户表';
 
-INSERT INTO `wxp_user` VALUES (1, 'admin', '管理员',  '21232f297a57a5a743894a0e4a801fc3', now(), '1', 0);
+INSERT INTO `wxp_user` VALUES (1, 'admin', '管理员',  '21232f297a57a5a743894a0e4a801fc3', now(), '1', 0, '', '0');
 
 
 
@@ -31,7 +33,7 @@ CREATE TABLE `wxp_group` (
   `group_id` tinyint(4) NOT NULL auto_increment,
   `group_name` varchar(50) NOT NULL COMMENT '组名称',
   `group_status` enum('1','0') NOT NULL COMMENT '组状态，1是启用，0是停用',
-  `group_auth` mediumint(8) unsigned NOT NULL COMMENT '组权限，关联authrule表的id字段',
+  `group_auth` char(80) NOT NULL COMMENT '组拥有的规则id， 多个规则","隔开',
   PRIMARY KEY (`group_id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='用户组表';
 
@@ -77,6 +79,7 @@ CREATE TABLE `wxp_resource` (
   `resource_checkdate` datetime NOT NULL COMMENT '资源审核通过日期',
   `resource_user` varchar(50) NOT NULL COMMENT '资源上传者，关联user表的user_id字段',
   `resource_checker` varchar(50) NOT NULL COMMENT '资源审核者，关联user表的user_id字段',
+  `resource_weixin` varchar(50) NOT NULL COMMENT '资源是哪个公众号的，关联weixin表weixin_number字段',
   `resource_printer` int(11) unsigned NOT NULL COMMENT '资源在哪台设备打印，关联printer表的printer_id字段',
   PRIMARY KEY (`resource_id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='资源表';
@@ -90,7 +93,7 @@ CREATE TABLE `wxp_printer` (
   `printer_code` varchar(20) NOT NULL COMMENT '打印机消费码',
   `printer_imgcode` varchar(50) NOT NULL COMMENT '打印机二维码',
   `printer_type` enum('1','2') NOT NULL COMMENT '打印机终端类型，1是横屏，2是竖屏',
-  `printer_weixin` smallint(6) unsigned NOT NULL COMMENT '公众号，关联weixin表weixin_id字段',
+  `printer_weixin` varchar(50) NOT NULL COMMENT '公众号帐号，关联weixin表weixin_number字段',
   PRIMARY KEY (`printer_id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='设备表';
 
@@ -99,9 +102,10 @@ CREATE TABLE `wxp_printer` (
 DROP TABLE IF EXISTS `wxp_weixin`;
 CREATE TABLE `wxp_weixin` (
   `weixin_id` smallint(6) NOT NULL auto_increment,
-  `weixin_number` varchar(50) NOT NULL COMMENT '公众号',
+  `weixin_number` varchar(50) NOT NULL COMMENT '公众号帐号',
   `weixin_callbackurl` varchar(200) NOT NULL COMMENT '回调地址',
   `weixin_token` varchar(50) NOT NULL COMMENT 'token',
-  PRIMARY KEY (`weixin_id`)
+  PRIMARY KEY (`weixin_id`),
+  UNIQUE KEY weixin_number (weixin_number)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='公众号表';
 
