eaf27c6060c10241475c8011d5a26292db428484
zhaobin <zhaobin@deepin.deepin-domain>
2014-10-08 17:38:42 +0800

    add

--- Application/Home/Model/PrintcodeModel.class.php
+++ Application/Home/Model/PrintcodeModel.class.php
@@ -3,14 +3,14 @@ namespace Home\Model;
 
 class PrintcodeModel extends BaseModel {
 
-    pubcli function getCode($code) {
+    public function getCode($code) {
         $where['p_code_number'] = array('like', $code.'%');
         $where['p_status'] = 0;
         $info = $this->where($where)->find();
         return $info['p_code_number'];
     }
 
-    pubcli function createCode($code) {
+    public function createCode($code) {
         $where['p_code_number'] = array('like', $code.'%');
         $where['p_status'] = 0;
         $info = $this->where($where)->setField('p_status','1');
