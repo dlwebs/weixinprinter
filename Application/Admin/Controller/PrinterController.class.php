df2a103482cb8a6f28cb08e9593e7a81afe46018
ivan820819 <ivan820819@qq.com>
2014-10-12 15:32:06 +0800

    zb

--- Application/Admin/Controller/PrinterController.class.php
+++ Application/Admin/Controller/PrinterController.class.php
@@ -7,23 +7,23 @@ class PrinterController extends BaseController {
         $printerobj = D('printer');
         $searchArray = I('post.');
         if(count($searchArray)){
-			$condition = " 1 ";
-			if($searchArray["search_name"]){
-				$condition = $condition." AND printer_name like '%".$searchArray["search_name"]."%'";
-				$this->assign('search_name', $searchArray["search_name"]);
-			}
-			if($searchArray["search_weixin"]){
-				$condition = $condition." AND printer_weixin like '%".$searchArray["search_weixin"]."%'";
-				$this->assign('search_weixin', $searchArray['search_weixin']);
-			}
-			if($searchArray["search_type"]){
-				$condition = $condition." AND printer_type = '".$searchArray["search_type"]."'";
-				$this->assign('search_type', $searchArray['search_type']);
-			}
+            $condition = " 1 ";
+            if($searchArray["search_name"]){
+                $condition = $condition." AND printer_name like '%".$searchArray["search_name"]."%'";
+                $this->assign('search_name', $searchArray["search_name"]);
+            }
+            if($searchArray["search_weixin"]){
+                $condition = $condition." AND printer_weixin like '%".$searchArray["search_weixin"]."%'";
+                $this->assign('search_weixin', $searchArray['search_weixin']);
+            }
+            if($searchArray["search_type"]){
+                $condition = $condition." AND printer_type = '".$searchArray["search_type"]."'";
+                $this->assign('search_type', $searchArray['search_type']);
+            }
         }
-		if($condition){
-			$printerdata = $printerobj->getPrinterList('', $condition);
-		}else{
+        if($condition){
+            $printerdata = $printerobj->getPrinterList('', $condition);
+        }else{
             $printerdata = $printerobj->getPrinterList();
         }
         $this->assign('printerlist', $printerdata['data']);
@@ -42,9 +42,9 @@ class PrinterController extends BaseController {
         $printer_id = I('get.printerid');
         $printerobj = D("printer");
         $printerinfo = $printerobj->getPrinterById($printer_id);
-		$this->assign('printerinfo', $printerinfo);
+        $this->assign('printerinfo', $printerinfo);
 
-		$weixin = D('weixin');
+        $weixin = D('weixin');
         $weixindata = $weixin->getWeixinList('all');
         $this->assign('weixinlist', $weixindata['data']);
 
@@ -67,54 +67,38 @@ class PrinterController extends BaseController {
     }
 
     public function saveAction() {
-		$post = I('post.');
+        $post = I('post.');
         $printerobj = D('printer');
         if (isset($post['id']) && $post['id']) {
-			if (!$post['printer_name']) {
-				$this->error("打印机名称不能为空");
-			}
-			$pninfo = $printerobj->getPrinterByName($post['printer_name']);
-			if (count($pninfo) && $pninfo["printer_id"] != $post['printer_id']) {
-				$this->error("打印机名称已存在");
-			}
-			if (!$post['printer_code']) {
-				$this->error("打印机消费码不能为空");
-			}
-			$pcinfo = $printerobj->getPrinterByCode($post['printer_code']);
-			if (count($pcinfo) && $pcinfo["printer_id"] != $post['printer_id']) {
-				$this->error("打印机消费码已存在");
-			}
-			if(!$post['printer_weixin']){
-				$this->error("公共帐号不能为空");
-			}
-			$pxinfo = $printerobj->getPrinterByWeixin($post['printer_weixin']);
-			if (count($pxinfo) && $pxinfo["printer_id"] != $post['printer_id']) {
-				$this->error("公众帐号已存在");
-			}
+            if (!$post['printer_name']) {
+                $this->error("打印机名称不能为空");
+            }
+            if (!$post['printer_code']) {
+                $this->error("打印机消费码不能为空");
+            }
+            $pcinfo = $printerobj->getPrinterByCode($post['printer_code']);
+            if (count($pcinfo) && $pcinfo["printer_id"] != $post['printer_id']) {
+                $this->error("打印机消费码已存在");
+            }
+            if(!$post['printer_weixin']){
+                $this->error("公共帐号不能为空");
+            }
             $printernumber = $printerobj->updatePrinter($post);
             $id = $post['id'];
         } else {
-			if (!$post['printer_name']) {
-				$this->error("打印机名称不能为空");
-			}
-			$pninfo = $printerobj->getPrinterByName($post['printer_name']);
-			if ($pninfo) {
-				$this->error("打印机名称已存在");
-			}
-			if (!$post['printer_code']) {
-				$this->error("打印机消费码不能为空");
-			}
-			$pcinfo = $printerobj->getPrinterByCode($post['printer_code']);
-			if ($pcinfo) {
-				$this->error("打印机消费码已存在");
-			}
-			if(!$post['printer_weixin']){
-				$this->error("公共帐号不能为空");
-			}
-			$pxinfo = $printerobj->getPrinterByWeixin($post['printer_weixin']);
-			if ($pxinfo) {
-				$this->error("公众帐号已存在");
-			}
+            if (!$post['printer_name']) {
+                $this->error("打印机名称不能为空");
+            }
+            if (!$post['printer_code']) {
+                $this->error("打印机消费码不能为空");
+            }
+            $pcinfo = $printerobj->getPrinterByCode($post['printer_code']);
+            if ($pcinfo) {
+                $this->error("打印机消费码已存在");
+            }
+            if(!$post['printer_weixin']){
+                $this->error("公共帐号不能为空");
+            }
             $id = $printerobj->addPrinter($post);
         }
         if ($id) {
