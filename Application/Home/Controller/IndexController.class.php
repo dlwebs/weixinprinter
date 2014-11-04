1145c66e2e2a91e82dd9d7d1eb368a41dad60a7b
zhaobin <zhaobin@deepin.deepin-domain>
2014-11-04 13:55:33 +0800

    add

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -29,16 +29,31 @@ class IndexController extends BaseController {
             foreach ($printer_content as $value) {
                 if ($value['printertpl_type'] == 'video') {
                     $video[] = $value['printertpl_content'];
+                    $this->assign('video'.$value['printertpl_num'], '<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" 
+                                                                                                                        codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,29,0" height="120" width="190"> 
+                                                                                                                        <param name="movie" 
+                                                                                                                        value="http://'.$_SERVER['SERVER_NAME'].'/upload/'.$value['printertpl_content'].'"> 
+                                                                                                                        <param name="quality" value="high"> 
+                                                                                                                        <param name="allowFullScreen" value="true" /> 
+                                                                                                                        <embed 
+                                                                                                                        src="http://'.$_SERVER['SERVER_NAME'].'/upload/'.$value['printertpl_content'].'" 
+                                                                                                                        quality="high" 
+                                                                                                                        pluginspage="http://www.macromedia.com/go/getflashplayer" 
+                                                                                                                        type="application/x-shockwave-flash" width="320" height="240"> 
+                                                                                                                        </embed> 
+                                                                                                                    </object>');
                 } elseif ($value['printertpl_type'] == 'image') {
                     $image[] = $value['printertpl_content'];
+                    $this->assign('image'.$value['printertpl_num'], '<img src="http://'.$_SERVER['SERVER_NAME'].'/upload/'.$value['printertpl_content'].'" width="100%" height="100%">');
                 } else {
                     $word[] = $value['printertpl_content'];
+                    $this->assign('word'.$value['printertpl_num'], $value['printertpl_content']);
                 }
             }
 
-            $this->assign('printer_id', $printer_id);
-            $this->assign('weixin_imgcode', $wxinfo['weixin_imgcode']);
-            $this->assign('avalible_code', $code);
+            $this->assign('printer', $printerInfo);
+            $this->assign('imgcode', '<img src="http://'.$_SERVER['SERVER_NAME'].'/upload/'.$wxinfo['weixin_imgcode'].'" width="100%" height="100%">');
+            $this->assign('code', $code);
             $this->assign('video', $video);
             $this->assign('image', $image);
             $this->assign('word', $word);
