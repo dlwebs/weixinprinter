25b81639693c81ad5873f4c9f03555d0b82119fc
zhaobin <zhaobin@deepin.deepin-domain>
2014-11-27 11:11:10 +0800

    add

--- Application/Home/Controller/ServiceController.class.php
+++ Application/Home/Controller/ServiceController.class.php
@@ -5,8 +5,8 @@ use Think\Controller\RestController;
 
 class ServiceController extends RestController {
 
-    public function regprinterAction_post(){
-        $activecode = I('post.activecode');
+    public function regprinterAction_get(){
+        $activecode = I('get.activecode');
         $printobj = new \Admin\Model\PrinterModel();
         $result = $printobj->activePrinter($activecode);
         if ($result == 1) {
