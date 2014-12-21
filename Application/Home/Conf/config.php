0f23ce3f1f8bc4bb5227016dd03b5e471a9ecd0d
mz121star <mz121star@gmail.com>
2014-12-21 10:55:32 +0800

    杂志风格照片

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -10,7 +10,7 @@ $private_config = array(
                         'URL_ROUTE_RULES' => array(
                                                   'api/:token' => 'Weixin/index',
                                                   'zoom/:uid' => 'Weixin/zoom',
-                                                   'zoom/:uid' => 'Weixin/zoom',
+                                                   'zoom2/:uid' => 'Weixin/zoom',
                                                   'printer/:atcode' => 'Index/index',
                                                   )
                         );
